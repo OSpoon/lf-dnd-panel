084908229ae76069a9b9d40f5858a4a09d760f33
fe-xiaoxin <zxin088@gmail.com>
2024-03-25 14:12:08 +0800

    feat:  利用 h 函数,将 AntdV 组件库的 collapse 接入拖拽面板

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 import { onMounted, ref } from "vue";
 import LogicFlow from "@logicflow/core";
 import { DndPanel } from "./extension/dnd-panel";
+import { DndPanelVue } from "./extension/dnd-panel-vue";
 import "@logicflow/core/dist/style/index.css";
 import "@logicflow/extension/lib/style/index.css";
 
 import { icons } from "./icons";
+
+import VueNode from "./components/VueNode/index";
+import AntdVNode from "./components/AntdVNode/index";
 
 const container = ref();
 
@@ -13,16 +17,35 @@ onMounted(() => {
   const lf = new LogicFlow({
     container: container.value,
     grid: true,
-    plugins: [DndPanel],
+    plugins: [DndPanel, DndPanelVue],
   });
 
-  lf.extension.dndPanel.setPatternItems([
+  lf.register(VueNode);
+  lf.register(AntdVNode);
+  lf.extension.dndPanelVue.setPatternItems([
     {
       group: "功能节点",
       items: [
         {
           label: "选区",
           icon: icons.select,
+        },
+        {
+          type: "vue-node",
+          label: "Vue节点",
+          icon: icons.select,
+          properties: {
+            innerText: "Vue节点",
+          },
+        },
+        {
+          type: "antdv-node",
+          label: "AntdV节点",
+          icon: icons.select,
+          properties: {
+            innerText: "AntdV节点",
+            type: "primary",
+          },
         },
       ],
     },
@@ -58,6 +81,10 @@ onMounted(() => {
   ]);
 
   lf.render();
+
+  lf.on("vue-node:onClick", (e) => {
+    console.log(e);
+  });
 });
 </script>
 
